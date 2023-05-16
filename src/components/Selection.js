279482237e840fbf9a67eec4842afff88fbbfe18
anjupatil <anjupatil42@gmail.com>
2023-05-17 03:10:03 +0530

    submitting code-Wed May 17 2023 03:10:02 GMT+0530 (India Standard Time)

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,21 @@
 
+import React from 'react';
+
+const Selection = ({ applyColor, nextBackground }) => {
+  const handleClick = () => {
+    applyColor(nextBackground);
+  };
+
+  return (
+    <div className="selection">
+      <div
+        className="box"
+        style={{ background: nextBackground.background }}
+        onClick={handleClick}
+      ></div>
+      <button onClick={handleClick}>Apply Color</button>
+    </div>
+  );
+};
+
+export default Selection;
